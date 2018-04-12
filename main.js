6030e916be5d66908354921907b969c4a10334e3
SunnySunMoon <504415750@qq.com>
2018-04-12 18:43:27 +0800

    correct paths building

--- main.js
+++ main.js
@@ -19,16 +19,22 @@ StSMap.prototype.createNodesNum = function () {
                 next1: null, //every node can split into two paths
                 next2: null,
                 level: i+1,
-                connected: false,
+                beConnected: false,
                 id: id++
             });
         }
         this.levelsArr.push(level);
     }
     for( let i=0; i<this.levelsArr[0].length; i++) {
-        this.levelsArr[0][i].connected = true;
+        this.levelsArr[0][i].beConnected = true;
     }
-    this.levelsArr.push([{type: 5, level: this.length, connected: true}]);
+    this.levelsArr.push([{
+        type: 5, 
+        level: this.length, 
+        beConnected: true,
+        toConnect: true,
+        id: 'boss',
+    }]);
 }
 
 //assign a type to the node
@@ -46,26 +52,30 @@ StSMap.prototype.buildPaths = function () {
             //if this node has connected to two nodes in last level, it cannot be connected from last level's remaining nodes any longer
         }; 
         for (let j=0; j<this.levelsArr[i].length; j++) {
-            if (!this.levelsArr[i][j].connected && i!=0) {
+            if (!this.levelsArr[i][j].beConnected && i!=0) {
                 break;
             }
             //firstly, determine whether this node will connect to the new node in the next level.
-            if ((Math.random() <= 0.25 || nodeConnected.connectedNodesNum==2) && j!=0) { //it will
+            if ((Math.random() <= 0.1 || nodeConnected.connectedNodesNum==2) && j!=0) { //it will
                 nodeConnected.index++;
                 nodeConnected.connectedNodesNum = 0;
             } else { } //it won't, then keep this index
 
             //secondly, determine whether this node will split its path
-            if (Math.random() <= 0.25) { //it will
+            if (Math.random() <= 0.45) { //it will
                 this.levelsArr[i][j].next1 = this.levelsArr[i+1][nodeConnected.index];
-                this.levelsArr[i+1][nodeConnected.index].connected = true;
+                if (this.levelsArr[i+1][nodeConnected.index]) {
+                    this.levelsArr[i+1][nodeConnected.index].beConnected = true;
+                }
                 nodeConnected.index++;
                 if (nodeConnected.index > 4) {
                     break;
                 }
                 this.levelsArr[i][j].next2 = this.levelsArr[i+1][nodeConnected.index];
-                console.log(nodeConnected.index)
-                this.levelsArr[i+1][nodeConnected.index].connected = true;
+                if (nodeConnected.index > 4) {
+                    break;
+                }
+                this.levelsArr[i+1][nodeConnected.index].beConnected = true;
                 nodeConnected.connectedNodesNum = 1;
             } else { //it won't
                 this.levelsArr[i][j].next1 = this.levelsArr[i+1][nodeConnected.index];
@@ -73,14 +83,18 @@ StSMap.prototype.buildPaths = function () {
                 if (nodeConnected.index > 4) {
                     break;
                 }
-                this.levelsArr[i+1][nodeConnected.index].connected = true;
+                this.levelsArr[i+1][nodeConnected.index].beConnected = true;
+            }
+            if (this.levelsArr[i][j].next1) {
+                this.levelsArr[i][j].toConnect = true;
             }
         }
     }
     for (let i=0; i<this.levelsArr[this.levelsArr.length-2].length; i++) {
         //let all of the last but one level's nodes' next1 refer to the last node.
-        if (this.levelsArr[this.levelsArr.length-2][i].connected) {
+        if (this.levelsArr[this.levelsArr.length-2][i].beConnected) {
             this.levelsArr[this.levelsArr.length-2][i].next1 = this.levelsArr[this.levelsArr.length-1][0];
+            this.levelsArr[this.levelsArr.length-2][i].toConnect = true;
         }
     }
 }
@@ -89,7 +103,7 @@ StSMap.prototype.buildPaths = function () {
 StSMap.prototype.shakeTree = function () {
     for (let i=0; i<this.levelsArr.length; i++) {
         for (let j=0; j<this.levelsArr[i].length; j++) {
-            if (!this.levelsArr[i][j].connected) {
+            if (!this.levelsArr[i][j].beConnected || !this.levelsArr[i][j].toConnect) {
                 this.levelsArr[i].splice(j,1);
                 j--;
             }
@@ -104,16 +118,21 @@ StSMap.prototype.visualizeMap = function (dom) {
 
     let stage = new JTopo.Stage(dom);
     let scene = new JTopo.Scene(stage);
-    let nodeArr = [];
+    this.nodeArr = [];
     //visualize the nodes
     for (let i=0; i<this.levelsArr.length; i++) {
         for (let j=0; j<this.levelsArr[i].length; j++) {
-            let node = new JTopo.CircleNode(this.levelsArr[i][j].id);
+            let node = new JTopo.Node(this.levelsArr[i][j].id);
             node.setLocation(j*120, i*140+30);
             node.fontColor = 'black';
             node.setSize(30,30);
             switch (this.levelsArr[i][j].type) {
-                //choose img
+                case 0 : node.setImage('./img/monster.png');break;
+                case 1 : node.setImage('img/elite.png');break;
+                case 2 : node.setImage('img/unknown.png');break;
+                case 3 : node.setImage('img/shop.png');break;
+                case 4 : node.setImage('img/chest.png');break;
+                case 5 : node.setImage('img/boss.png');node.setSize(200,200); break;
             }
             //set custom properties
             node.serializedProperties.push('next1');
@@ -127,28 +146,24 @@ StSMap.prototype.visualizeMap = function (dom) {
 
 
             scene.add(node);
-            nodeArr.push(node);
+            this.nodeArr.push(node);
         }
     };
+    console.log(this.nodeArr);
     //visualize the paths
-    for (let i=0; i<nodeArr.length; i++) {
-        if (nodeArr[i].next1) {
-            let nodeZ1 = scene.findElements(function (node) {
-                return node.text == nodeArr[i].next1;
-            });
-            let link1 = new JTopo.Link(nodeArr,nodeZ1[0]);
-            link1.dashedPattern = 5;
-            scene.add(link1);
+    for (let i=0; i<this.nodeArr.length; i++) {
+        for (let j=i; j<this.nodeArr.length; j++) {
+            if (this.nodeArr[i].next1 == this.nodeArr[j].text || this.nodeArr[i].next2 == this.nodeArr[j].text) {
+                createPath(this.nodeArr[i], this.nodeArr[j], scene);
+            }
         }
-        if (nodeArr[i].next2) {
-            let nodeZ2 = scene.findElements(function (node) {
-                return node.text = nodeArr[i].next2;
-            })
-            let link2 = new JTopo.Link(nodeArr[i],nodeZ2[0]);
-            link2.dashedPattern = 5;
-            scene.add(link2);
-        }
+    };
+    function createPath (nodeA, nodeZ, scene) {
+        let path = new JTopo.Link(nodeA,nodeZ);
+        path.dashedPattern = 5;
+        scene.add(path);
     }
 }
 
 let a = new StSMap();
+a.visualizeMap(document.getElementById('mapCanvas'));
